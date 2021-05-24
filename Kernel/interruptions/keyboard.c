45cd486eecb214193dd80525ca3372f11954dc1a
Pedro Jeremias Lopez Guzman <pelopez@itba.edu.ar>
2021-05-24 18:17:16 -0300

    Desde 0
    Tenemos que tener el proyecto vacio

--- Kernel/interruptions/keyboard.c
+++
@@ -1,8 +0,0 @@
-#include <keyboard.h>
-#include <./../include/naiveConsole.h>
-void keyboard_handler() {
-    unsigned int key = _getKey();
-    // aca podría hacer un mapeo, desp en el tpe lo haremos
-    ncPrintHex(key);
-    return;
-}
--- Kernel/video_driver.h
+++
@@ -1,7 +0,0 @@
-int str_len(char *str);
-void print_str(char *str, char color, int row, int column);
-void print_char(char c, char color, int row, int column);
-void print_num(int number, char color, int row, int column);
-void print(char *str, int length, char color, int row, int column);
-unsigned int count_digits(int number);
-void clear();
